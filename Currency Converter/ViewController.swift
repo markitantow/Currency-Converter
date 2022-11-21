d066cc133d595d855448092eea6bedb9cf325db6
Pavel Markitantov <pmarkitantov@gmail.com>
2022-11-21 01:04:31 +0100

    feat: add calculation

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var coef: Double = 0.0
+    var botCurrency = "USD"
+    var topCurrency = "USD"
     
+    @IBAction func changeTopSegment(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 1: topCurrency = "ZLT"
+        case 2: topCurrency = "BYN"
+        default: topCurrency = "USD"
+        }
+    }
     
-    
+    @IBAction func changeBotSegment(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 1: botCurrency = "ZLT"
+        case 2: botCurrency = "BYN"
+        default: botCurrency = "USD"
+        }
+    }
     
     @IBOutlet var topCurrencyCheck: UISegmentedControl!
     
@@ -22,80 +36,62 @@ class ViewController: UIViewController {
     
     @IBOutlet var botLabel: UILabel!
     @IBOutlet var topLabel: UILabel!
+    
     @IBAction func myButtonPressed(_ sender: Any) {
         
-        var topCurrency = ""
-        var botCurrency = ""
-        topLabel.text = userInput.text
-        topLabel.isHidden = false
-        
-        guard let text = userInput.text else {
-            return
-        }
-        
-        guard let number = Double(text) else {
-            return
-        }
-        
-        if topCurrencyCheck.isEnabledForSegment(at: 0) {
-            topCurrency = "USD"
-        }
-        
-        if topCurrencyCheck.isEnabledForSegment(at: 1) {
-            topCurrency = "ZLT"
-        }
-        
-        if topCurrencyCheck.isEnabledForSegment(at: 2) {
-            topCurrency = "BYN"
-        }
-        
-        if botCurrencyCheck.isEnabledForSegment(at: 0) {
-            botCurrency = "USD"
-        }
-        
-        if botCurrencyCheck.isEnabledForSegment(at: 1) {
-            botCurrency = "ZLT"
-        }
-        
-        if botCurrencyCheck.isEnabledForSegment(at: 2) {
-            botCurrency = "BYN"
-        }
-        
-        switch topCurrency {
-        case "USD": if botCurrency == "ZLT" {
-            coef = 4.55
+            topLabel.text = userInput.text
+            topLabel.isHidden = false
+            
+            guard let text = userInput.text else {
+                return
+            }
+            
+            guard let number = Double(text) else {
+                return
+            }
+            
+            
+            switch topCurrency {
+            case "USD": if botCurrency == "ZLT" {
+                coef = 0.22
             } else if botCurrency == "BYN" {
                 coef = 2.41
             } else {
                 coef = 1
             }
-            
-        case "ZLT": if botCurrency == "USD" {
-            coef = 0.22
+                
+            case "ZLT": if botCurrency == "USD" {
+                coef = 4.5588
             } else if botCurrency == "BYN" {
                 coef = 0.52
             } else {
                 coef = 1
             }
-        case "BYN": if botCurrency == "USD" {
-            coef = 0.41
+            case "BYN": if botCurrency == "USD" {
+                coef = 0.41
             } else if botCurrency == "ZLT" {
                 coef = 1.88
             } else {
                 coef = 1
             }
+                
+            default: print("")
+            }
             
-        default: print("")
+            let result = coef * number
+            botLabel.text = String(result)
+            botLabel.isHidden = false
+        if topLabel.text != nil {
+            topLabel.text! += " \(topCurrency)"
+            botLabel.text! += " \(botCurrency)"
         }
-
+        
     }
     
     override func viewDidLoad() {
         topLabel.isHidden = true
         botLabel.isHidden = true
-        
-        
-        
+
         super.viewDidLoad()
     }
 }
